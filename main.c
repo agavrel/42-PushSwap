7312e576ab5696323b2b8a07527b680afba80b7b
Tonyoh <gavrel.antonin@gmail.com>
2018-02-03 00:42:34 +0100

    [ADD] Adding shellSort algo and size of array given with av[1]

--- main.c
+++ main.c
@@ -4,10 +4,8 @@
 #include <string.h>
 #include <stdlib.h> // rand function
 
-#define SIZE 100000
-
 typedef struct  s_node {
-    int key;
+    size_t key;
     int value;
 }               t_node;
 
@@ -18,13 +16,40 @@ static inline void swap(t_node *a, t_node *b)
     *b = t;
 }
 
-static inline void insertionSort(t_node arr[]) {
-    int j;
-    int i;
+static inline void shellSort(t_node arr[], size_t size)
+{
+    size_t gap = size / 2;
+    int tmp;
+    size_t j;
+    size_t i;
+
+    while (gap > 0)
+    {
+        i = gap;
+        while (i < size)
+        {
+            tmp = arr[i].value;
+            j = i;
+            while (j >= gap && arr[j - gap].value > tmp)
+            {
+                arr[j].value = arr[j - gap].value;
+                j -= gap;
+            }
+            arr[j].value = tmp;
+            ++i;
+        }
+        gap >>= 1;
+    }
+}
+
+static inline void insertionSort(t_node arr[], size_t size)
+{
+    size_t j;
+    size_t i;
     t_node tmp;
 
     i = 1;
-    while (i < SIZE)
+    while (i < size)
     {
         j = i++;
         tmp = arr[j];
@@ -35,12 +60,13 @@ static inline void insertionSort(t_node arr[]) {
     }
 }
 
-static inline void maxHeapify(t_node arr[], int heapSize, int index)
+static inline void maxHeapify(t_node arr[], size_t heapSize, size_t index)
 {
-    int right = (index + 1) * 2;
-    int left = right - 1;
-    int largest;
+    const size_t right = (index + 1) * 2;
+    size_t left;
+    size_t largest;
 
+    left = right - 1;
     largest = (left < heapSize && arr[left].value > arr[index].value) ? left : index;
     if (right < heapSize && arr[right].value > arr[largest].value)
         largest = right;
@@ -51,17 +77,17 @@ static inline void maxHeapify(t_node arr[], int heapSize, int index)
     }
 }
 
-static inline void heapSort(t_node arr[])
+static inline void heapSort(t_node arr[], size_t size)
 {
-    int heapSize;
-    int p;
-    int i;
+    size_t heapSize;
+    int    p;
+    size_t i;
 
-    p = (SIZE - 1) / 2;
+    p = (size - 1) / 2;
     while (p >= 0)
-        maxHeapify(arr, SIZE, p--);
-    heapSize = SIZE;
-    i = SIZE - 1;
+        maxHeapify(arr, size, p--);
+    heapSize = size;
+    i = size - 1;
     while (i > 0)
     {
         swap(&arr[i--], &arr[0]);
@@ -70,12 +96,12 @@ static inline void heapSort(t_node arr[])
 }
 
 
-static inline int partition(t_node arr[], int left, int right)
+static inline size_t partition(t_node arr[], size_t left, size_t right)
 {
     int pivot;
-    int i;
-    int j;
-    int tmp;
+    size_t i;
+    size_t j;
+//    int tmp;
 
     pivot = arr[right].value;
     i = left;
@@ -93,76 +119,82 @@ static inline int partition(t_node arr[], int left, int right)
     return (i);
 }
 
-static inline void quickSortRecursive(t_node arr[], int left, int right)
+static inline void quickSortRecursive(t_node arr[], size_t left, size_t right)
 {
+    size_t q;
+
     if (left < right)
     {
-        int q = partition(arr, left, right);
+        q = partition(arr, left, right);
         quickSortRecursive(arr, left, q - 1);
         quickSortRecursive(arr, q + 1, right);
     }
 }
 
-static inline void introSort(t_node arr[])
+static inline void introSort(t_node arr[], size_t size)
 {
-    int partitionSize;
+    const size_t partitionSize = partition(arr, 0, size - 1);
 
-    partitionSize = partition(arr, 0, SIZE - 1);
     if (partitionSize < 16)
-        insertionSort(arr);
-    else if (partitionSize > (log(SIZE) * 2))
-        heapSort(arr);
+        insertionSort(arr, size);
+    else if (partitionSize > (log(size) * 2))
+        heapSort(arr, size);
     else
-        quickSortRecursive(arr, 0, SIZE - 1);
+        quickSortRecursive(arr, 0, size - 1);
 }
 
-
-int main()
+static inline void testInit(size_t size)
 {
-//    int value[SIZE] = { 1, 2, 6, 15, 5, 19, 54, 0, -3, 7, 31, 75, -8, -4, 3, 55, 54, 21, 8, -9, 999, 99, 777, 88, 82, 56, 64, 128, 256, 512, 2048, 9898, 7979, 351, -543, 544, 546, 79797, 494090, 644645, 4564, 89798, -456465, 7897900, 4564654, 45465585, 456444444, 4479, -6464, -128, -64};
-    t_node  node[SIZE];
-    int     i;
-    int     value[SIZE];
+    t_node  node[size];
+    t_node  node2[size];
+    size_t  i;
+    int     value[size];
     int     rng;
 
     /* generate list filled with random numbers */
     srand(time(NULL));
     i = 0;
-    while (i < SIZE)
+
+    while (i < size)
     {
-        rng = rand() % SIZE;
+        rng = rand() % size;
         value[i] = (rng & 1) ? ~rng : rng;
         ++i;
     }
 
     /* set original index for each element of the array */
     i = 0;
-    while (i < SIZE)
+    while (i < size)
     {
         node[i].value = value[i];
         node[i].key = i;
         ++i;
     }
-
+    memcpy(node2, node, sizeof(t_node) * size);
     /* launch algo and time it */
-    clock_t start, end;
-    float elapsed_time;
+    clock_t start, end, start2, end2;
+
     start = clock();
-    introSort(node); // runs in about 0.34sd
+//    if (SIZE < 2000) // 2000 seems to be the size limit where introSort achieves better results then shellSort
+        shellSort(node, size);
     end = clock();
+//    else
+    start2 = clock();
+        introSort(node, size); // runs in about 0.34sd
+    end2 = clock();
 
     /* displaying result */
     i = 0;
-    while (i < SIZE)
+    while (i < size)
     {
-        printf("original index : %d, desired index: %d, value : %d\n", node[i].key, i, node[i].value);
+        printf("original index : %zu, desired index: %zu, value : %d\n", node[i].key, i, node[i].value);
         ++i;
     }
 
     /* check that list is correctly sorted */
     int fail = 0;
     i = 1;
-    while (i < SIZE - 1)
+    while (i < size - 1)
     {
         if (node[i - 1].value > node[i].value || node[i].value > node[i + 1].value) {
             fail = 1; break;
@@ -172,8 +204,21 @@ int main()
     printf(fail ? "FAIL\n" : "SUCCESS\n");
 
     /* micro-benchmarking time for algo */
-    elapsed_time = (float)(end - start) / (float)CLOCKS_PER_SEC;
-    printf("Elapsed time: %f seconds\n", elapsed_time);
+    float elapsed_time = (float)(end - start) / (float)CLOCKS_PER_SEC;
+    printf("Elapsed time for shellSort: %f seconds\n", elapsed_time);
+    float elapsed_time2 = (float)(end2 - start2) / (float)CLOCKS_PER_SEC;
+    printf("Elapsed time for introSort: %f seconds\n", elapsed_time2);
+}
 
-    return 0;
+int main(int ac, char **av)
+{
+//    int value[SIZE] = { 1, 2, 6, 15, 5, 19, 54, 0, -3, 7, 31, 75, -8, -4, 3, 55, 54, 21, 8, -9, 999, 99, 777, 88, 82, 56, 64, 128, 256, 512, 2048, 9898, 7979, 351, -543, 544, 546, 79797, 494090, 644645, 4564, 89798, -456465, 7897900, 4564654, 45465585, 456444444, 4479, -6464, -128, -64};
+    size_t  size;
+    if (ac != 2)
+        return (1);
+    size = atoi(av[1]);
+    if (size <= 0)
+        return (1);
+    testInit(size);
+    return (0);
 }
