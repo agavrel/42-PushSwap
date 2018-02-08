e3b5898a35a976380b88f4b31c7f2ea6fe6b8458
Antonin GAVREL <angavrel@e2r6p6.42.fr>
2018-02-08 15:57:31 +0100

    [FIX] push / pop from one list to another fully working

--- srcs/srcs_check/push.c
+++ srcs/srcs_check/push.c
@@ -6,88 +6,68 @@
 /*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 18:49:06 by angavrel          #+#    #+#             */
-/*   Updated: 2018/02/08 14:25:33 by angavrel         ###   ########.fr       */
+/*   Updated: 2018/02/08 15:57:07 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-static inline void  push_to_from(t_lst **to, t_lst **from)
+/*
+** clean links from the popped item
+*/
+
+static inline t_lst	*pop(t_lst **this)
 {
 	t_lst	*tmp;
 
-	if (!*to)
+	tmp = *this;
+	if ((*this)->next == *this)
+		*this = NULL;
+	else
 	{
-		*to = *from;
-		(*to)->next = *to;
-		(*to)->prev = *to;
+		(*this)->prev->next = (*this)->next;
+		(*this)->next->prev = (*this)->prev;
+		*this = (*this)->next;
 	}
-	else if (*to)
-	{
-		(*to)->next = *to;
-		(*to)->prev->next = *to;
-		*to = *from;
-	}
-	tmp = *from;
-	*from = (*from)->next;
-	if (*from)
-	{
-		(*from)->prev = tmp->prev;
-		(*from)->prev->next = *from;
-
-		(*from)->next->prev = *from;
-	}
+	tmp->prev = NULL;
+	tmp->next = NULL;
+	return (tmp);
 }
 
-/*
-t_lst	*tmp;
-
-if ((*this)->next)
+static inline void	push(t_lst **this, t_lst *item)
 {
-	tmp = *this;
-	*this = (*this)->next;
-	tmp->next = (*this)->next;
-	tmp->next->prev = tmp;
-	(*this)->prev = tmp->prev;
-	tmp->prev = *this;
-	(*this)->next = tmp;
-	(*this)->prev->next = *this;
-}*/
+	if (*this == NULL)
+	{
+		item->prev = item;
+		item->next = item;
+	}
+	else
+	{
+		item->prev = (*this)->prev;
+		item->next = (*this);
+		(*this)->prev->next = item;
+		(*this)->prev = item;
+	}
+	*this = item;
+}
+
+static inline void  push_from_to(t_lst **from, t_lst **to)
+{
+	t_lst	*item;
+
+	item = pop(from);
+	if (item)
+		push(to, item);
+}
 
 inline void			pa(t_lst **a, t_lst **b)
 {
 	if (*b)
-		push_to_from(a, b);
+		push_from_to(b, a);
 }
 
 inline void			pb(t_lst **a, t_lst **b)
 {
 	if (*a)
-		push_to_from(b, a);
+		push_from_to(a, b);
 }
-
-/*
-** create an empty node
-** The node after the one that was the last is the newly created node
-** The node after the newly created is the first one
-** The node before the newly created is the former last one
-** The node before the first is the newly created one
-*/
-/*
-static inline void	lst_add(t_lst *lst, int value)
-{
-	t_lst	*tmp;
-
-	tmp = lst_create(value);
-	if (!lst)
-		lst = tmp;
-	else
-	{
-		if (lst->prev)
-			lst->prev->next = tmp;
-		tmp->next = lst;
-		tmp->prev = lst->prev;
-		lst->prev = tmp;
-	}
-}
-*/
